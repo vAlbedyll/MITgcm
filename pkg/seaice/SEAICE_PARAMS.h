6c3f8fc6b4d8240660be6a1211e36bf1e1f254ae
Luisa von Albedyll <lalbedyl@bkli04l006.dmawi.de>
2020-02-03 13:29:39 +0100

    fix too long line, remove whitespaces and empty line

--- pkg/seaice/SEAICE_PARAMS.h
+++ pkg/seaice/SEAICE_PARAMS.h
@@ -303,10 +303,11 @@ C     HsaltFile         :: File containing initial sea ice salt content
 C     HeffFile          :: File containing initial sea-ice thickness
 C     uIceFile          :: File containing initial sea-ice U comp. velocity
 C     vIceFile          :: File containing initial sea-ice V comp. velocity
-C     AreaITDfile       :: File containing initial sea-ice concentration distribution
+C     AreaITDfile       :: File containing initial sea-ice concentration
+C                          distribution
 C     HsnowITDfile      :: File containing initial snow thickness distribution
-C     HeffITDfile       :: File containing initial sea-ice thickness distribution
-
+C     HeffITDfile       :: File containing initial sea-ice thickness
+C                          distribution
 C        !!! NOTE !!! Initial sea-ice thickness can also be set using
 C        SEAICE_initialHEFF below.  But a constant initial condition
 C        can mean large artificial fluxes of heat and freshwater in
@@ -323,7 +324,8 @@ C
       CHARACTER*(MAX_LEN_FNAM) HeffITDfile
       COMMON /SEAICE_PARM_C/
      &   AreaFile, HsnowFile, HsaltFile, HeffFile,
-     &   uIceFile, vIceFile,  AreaITDfile, HsnowITDfile, HeffITDfile
+     &   uIceFile, vIceFile,
+     &   AreaITDfile, HsnowITDfile, HeffITDfile
 
 C--   COMMON /SEAICE_PARM_RL/ Real valued parameters of sea ice model.
 C     SEAICE_deltaTtherm :: Seaice timestep for thermodynamic equations (s)
